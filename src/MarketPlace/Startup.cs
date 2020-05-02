c53e26151002b76d1f07e39cbd840ed88faf536b
Daniel Wellner <daniel.wellner@sysart.fi>
2020-05-02 10:02:44 +0300

    remamed webapp to make it come first in the project listing

--- src/MarketPlace/Startup.cs
+++ src/MarketPlace/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace MarketPlace.Web
+namespace MarketPlace
 {
     public class Startup
     {
